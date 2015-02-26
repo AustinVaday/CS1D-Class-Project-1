5f32c0bf99aeaf8ad6182e1b8aa05925f8bd2228
Aurel <Aurel@Aurel-PC.attlocal.net>
2015-02-25 21:43:30 -0800

    -scroll area and checkbox

---
+++ build-CS1D-Class-Project-1-Desktop_Qt_5_4_0_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,188 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.4.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../WineryProject/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.4.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[21];
+    char stringdata[500];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 23), // "on_actionHelp_triggered"
+QT_MOC_LITERAL(2, 35, 0), // ""
+QT_MOC_LITERAL(3, 36, 21), // "on_quitButton_clicked"
+QT_MOC_LITERAL(4, 58, 25), // "on_adminLogButton_clicked"
+QT_MOC_LITERAL(5, 84, 33), // "on_viewListWineriesButton_cli..."
+QT_MOC_LITERAL(6, 118, 28), // "on_planDayTripButton_clicked"
+QT_MOC_LITERAL(7, 147, 29), // "on_tourWineriesButton_clicked"
+QT_MOC_LITERAL(8, 177, 30), // "on_admin_log_ok_button_clicked"
+QT_MOC_LITERAL(9, 208, 35), // "on_adming_log_cancel_button_c..."
+QT_MOC_LITERAL(10, 244, 31), // "on_admin_log_out_button_clicked"
+QT_MOC_LITERAL(11, 276, 19), // "on_ViewBack_clicked"
+QT_MOC_LITERAL(12, 296, 23), // "on_PlanTripBack_clicked"
+QT_MOC_LITERAL(13, 320, 19), // "on_TourBack_clicked"
+QT_MOC_LITERAL(14, 340, 20), // "on_LogInBack_clicked"
+QT_MOC_LITERAL(15, 361, 21), // "on_listView_activated"
+QT_MOC_LITERAL(16, 383, 5), // "index"
+QT_MOC_LITERAL(17, 389, 25), // "on_listWidget_2_activated"
+QT_MOC_LITERAL(18, 415, 36), // "on_page_view_list_wineries_de..."
+QT_MOC_LITERAL(19, 452, 23), // "on_scrollArea_destroyed"
+QT_MOC_LITERAL(20, 476, 23) // "on_pushButton_2_clicked"
+
+    },
+    "MainWindow\0on_actionHelp_triggered\0\0"
+    "on_quitButton_clicked\0on_adminLogButton_clicked\0"
+    "on_viewListWineriesButton_clicked\0"
+    "on_planDayTripButton_clicked\0"
+    "on_tourWineriesButton_clicked\0"
+    "on_admin_log_ok_button_clicked\0"
+    "on_adming_log_cancel_button_clicked\0"
+    "on_admin_log_out_button_clicked\0"
+    "on_ViewBack_clicked\0on_PlanTripBack_clicked\0"
+    "on_TourBack_clicked\0on_LogInBack_clicked\0"
+    "on_listView_activated\0index\0"
+    "on_listWidget_2_activated\0"
+    "on_page_view_list_wineries_destroyed\0"
+    "on_scrollArea_destroyed\0on_pushButton_2_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      18,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  104,    2, 0x08 /* Private */,
+       3,    0,  105,    2, 0x08 /* Private */,
+       4,    0,  106,    2, 0x08 /* Private */,
+       5,    0,  107,    2, 0x08 /* Private */,
+       6,    0,  108,    2, 0x08 /* Private */,
+       7,    0,  109,    2, 0x08 /* Private */,
+       8,    0,  110,    2, 0x08 /* Private */,
+       9,    0,  111,    2, 0x08 /* Private */,
+      10,    0,  112,    2, 0x08 /* Private */,
+      11,    0,  113,    2, 0x08 /* Private */,
+      12,    0,  114,    2, 0x08 /* Private */,
+      13,    0,  115,    2, 0x08 /* Private */,
+      14,    0,  116,    2, 0x08 /* Private */,
+      15,    1,  117,    2, 0x08 /* Private */,
+      17,    1,  120,    2, 0x08 /* Private */,
+      18,    0,  123,    2, 0x08 /* Private */,
+      19,    0,  124,    2, 0x08 /* Private */,
+      20,    0,  125,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QModelIndex,   16,
+    QMetaType::Void, QMetaType::QModelIndex,   16,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        switch (_id) {
+        case 0: _t->on_actionHelp_triggered(); break;
+        case 1: _t->on_quitButton_clicked(); break;
+        case 2: _t->on_adminLogButton_clicked(); break;
+        case 3: _t->on_viewListWineriesButton_clicked(); break;
+        case 4: _t->on_planDayTripButton_clicked(); break;
+        case 5: _t->on_tourWineriesButton_clicked(); break;
+        case 6: _t->on_admin_log_ok_button_clicked(); break;
+        case 7: _t->on_adming_log_cancel_button_clicked(); break;
+        case 8: _t->on_admin_log_out_button_clicked(); break;
+        case 9: _t->on_ViewBack_clicked(); break;
+        case 10: _t->on_PlanTripBack_clicked(); break;
+        case 11: _t->on_TourBack_clicked(); break;
+        case 12: _t->on_LogInBack_clicked(); break;
+        default: ;
+        }
+    }
+
+
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 18)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 18;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
